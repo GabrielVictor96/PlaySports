c33f5a1426fac40ff9041b94960b3b6c10de40ac
Gabriel Victor Ramalho <gvr_1996@hotmail.com>
2019-06-13 17:52:50 -0300

    teste

--- src/Mutant.Test.Infra.Data/Repository/AgendaRepository.cs
+++ src/Mutant.Test.Infra.Data/Repository/AgendaRepository.cs
@@ -7,7 +7,6 @@ using PlaySports.Infra.Data.Context;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PlaySports.Infra.Data.Repository
--- src/Mutant.Test.UI.MvcCore/Controllers/ChatController.cs
+++ src/Mutant.Test.UI.MvcCore/Controllers/ChatController.cs
@@ -93,73 +93,18 @@ namespace PlaySports.Controllers
             return View();
         }
 
-        // GET: Chat/Create
-        public ActionResult Create()
+        [HttpGet]
+        [Route("{id}/delete")]
+        public ActionResult Delete(Guid? id)
         {
+
+
+
+
+
+
+
             return View();
-        }
-
-        // POST: Chat/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Chat/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Chat/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Chat/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Chat/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
